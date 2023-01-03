29a6c2526e4971931a9104f7d0d9d89c8306abd6
ogc1231 <ogc@live.ie>
2023-01-03 12:23:36 +0000

    Add listner to call playSound fnc when btn pressed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,4 +2,7 @@ const buttons = document.querySelectorAll(".button")
 
 buttons.forEach(function(button) {
     console.log(button)
+    button.addEventListener("click", function() {
+        playSound(button)
+    } )
 })
