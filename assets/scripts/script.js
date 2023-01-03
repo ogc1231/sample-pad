3db30c8b73514aa2a3dac3f89dd3ff3fa370e836
ogc1231 <ogc@live.ie>
2023-01-03 12:29:00 +0000

    Add comments to make script more understandable

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,14 +1,21 @@
+// calling elements with button class into script
 const buttons = document.querySelectorAll(".button")
 
 buttons.forEach(function(button) {
+    // logs elements called when button clicked to console
     console.log(button)
+    // when button clicked calls playSound()
     button.addEventListener("click", function() {
         playSound(button)
     } )
 })
 
+// function play audio data files when called
 function playSound(button) {
+    // pull in audio data
     const soundAudio = document.getElementById(button.dataset.sound)
-    soundAudio.currentTime = 0
+    // audio restarts when button pressed again
+    soundAudio.currentTime = 0 
+    // plays audio file
     soundAudio.play()
 }
