00e258f1eca54dd3fc7a78916182c2b1795416fb
ogc1231 <ogc@live.ie>
2023-01-04 13:47:00 +0000

    Add accordion functionality

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,21 +1,53 @@
-// calling elements with button class into script
-const buttons = document.querySelectorAll(".button")
+// // calling elements with button class into script
+// const buttons = document.querySelectorAll(".button")
 
-buttons.forEach(function(button) {
-    // logs elements called when button clicked to console
-    console.log(button)
-    // when button clicked calls playSound()
-    button.addEventListener("click", function() {
-        playSound(button)
-    } )
+// buttons.forEach(function(button) {
+//     // logs elements called when button clicked to console
+//     console.log(button)
+//     // when button clicked calls playSound()
+//     button.addEventListener("click", function() {
+//         playSound(button)
+//     } )
+// })
+
+// // function play audio data files when called
+// function playSound(button) {
+//     // pull in audio data
+//     const soundAudio = document.getElementById(button.dataset.sound)
+//     // audio restarts when button pressed again
+//     soundAudio.currentTime = 0 
+//     // plays audio file
+//     soundAudio.play()
+// }
+
+const synthButton = document.querySelectorAll(".button")
+
+let audio = new Audio("assets/audio/kick.wav")
+
+const playTune = (button) => {
+    audio.src = `assets/audio/${button}.wav`
+    audio.play()
+
+    const clickedKey = document.querySelector(`[data-key="${button}"]`)
+    clickedKey.classList.add("active")
+}
+
+synthButton.forEach(button => {
+    button.addEventListener("click", () => playTune(button.dataset.sound))
 })
 
-// function play audio data files when called
-function playSound(button) {
-    // pull in audio data
-    const soundAudio = document.getElementById(button.dataset.sound)
-    // audio restarts when button pressed again
-    soundAudio.currentTime = 0 
-    // plays audio file
-    soundAudio.play()
+const pressedKey = (e) => {
+    playTune(e.button)
 }
+
+document.addEventListener("keydown", pressedKey)
+
+// Accordion menu
+
+const question = document.querySelectorAll(".question")
+
+question.forEach(question => {
+  question.addEventListener("click", event => {
+    question.classList.toggle("active");    
+  });
+});
