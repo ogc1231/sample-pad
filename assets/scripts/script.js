1b9a01effcb490ed4a1eee3a13ce4fc415afe584
ogc1231 <ogc@live.ie>
2023-01-08 15:07:51 +0000

    Add semicolons where needed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,58 +1,58 @@
 /* jshint esversion: 11 */
 
-const synthButton = document.querySelectorAll(".button")
-volume = document.querySelector(".volume input")
+const synthButton = document.querySelectorAll(".button");
+volume = document.querySelector(".volume input");
 
-let audio = new Audio("assets/audio/kick.wav")
+let audio = new Audio("assets/audio/kick.wav");
 
 let clickedKey;
 
 const playTune = (button) => {
-  clickedKey = document.querySelector(`[data-key="${button}"]`)
-  clickedKey.classList.add("active")
-  let dataSound = clickedKey.dataset.sound
-  audio.src = `assets/audio/${dataSound}.wav`
-  audio.currentTime = 0 
-  audio.play()
+  clickedKey = document.querySelector(`[data-key="${button}"]`);
+  clickedKey.classList.add("active");
+  let dataSound = clickedKey.dataset.sound;
+  audio.src = `assets/audio/${dataSound}.wav`;
+  audio.currentTime = 0 ;
+  audio.play();
 
   // audio.addEventListener("ended", function() {
-  //   clickedKey.classList.remove("active")
-  // })
+  //   clickedKey.classList.remove("active");
+  // });
   setTimeout(() => {
-    clickedKey.classList.remove("active")
+    clickedKey.classList.remove("active");
   }, 100);
-}
+};
 
 synthButton.forEach(button => {
-  button.addEventListener("click", () => playTune(button.dataset.key))
-})
+  button.addEventListener("click", () => playTune(button.dataset.key));
+});
 
 const handleVolume = (e) => {
   audio.volume = e.target.value / 100;
-}
+};
 
 const pressedKey = (e) => {
-  playTune(e.key)
-}
+  playTune(e.key);
+};
 
-volume.addEventListener("input", handleVolume)
-document.addEventListener("keydown", pressedKey)
+volume.addEventListener("input", handleVolume);
+document.addEventListener("keydown", pressedKey);
 
 // Accordion menu
 
-const questions = document.querySelectorAll(".question")
+const questions = document.querySelectorAll(".question");
 
 questions.forEach(question => {
   question.addEventListener("click", event => {
     questions.forEach(question => {
       question.classList.toggle("active");
-    })
-    console.log(question.classList)
+    });
+    console.log(question.classList);
     if (question.classList.contains("active")) {
-      console.log("a")
+      console.log("a");
       question.classList.add("active");
     } else {
-      console.log("b")
+      console.log("b");
       question.classList.re("active");
     }
   });
